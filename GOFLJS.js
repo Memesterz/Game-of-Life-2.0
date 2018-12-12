e2afab0c17a6df75c5d3c2c9e66082e080721304
Memesterz <31660848+Memesterz@users.noreply.github.com>
2018-12-12 14:07:52 -0500

    Update GOFLJS.js

--- GOFLJS.js
+++ GOFLJS.js
@@ -1,1 +1,23 @@
+function start() {
+    var width = screen.width / 2;
+    var height = screen.height / 2;
+    var menuJS = document.getElementById("gameScreen");
+    //menuJS.style.marginTop = height;
+    //menuJS.style.marginLeft = width;
+    
+}
 
+function statLimit() {
+    var alertJS = document.getElementById("alertMSG");
+}
+
+function Vars() {
+var HPJS = document.getElementById("health");
+}
+
+function task1_Clicked() {
+    Vars();
+    var alertJS = document.getElementById("alertMSG");
+        alertJS.innerHTML = "You went to the Hospital it wasn't a pleasant trip.";
+        alert(HPJS.innerHTML);
+    }
